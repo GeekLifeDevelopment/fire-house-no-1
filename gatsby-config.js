6d67e9677ad8f8eb239fe6ce0dffb40322c41e45
GeekLifeDevelopment <jason@geeklifedevelopment.com>
2023-08-28 22:27:29 -0500

    added contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,20 @@
 module.exports = {
   siteMetadata: {
     title: `FireHouseNo1`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "Z4bzDPxwNUcNYei6dZt-EpX9aLNRqCY1PyYXq4kUScI",
-      "spaceId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components"]
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: `atehm0n85i7a`,
+        accessToken: `Z4bzDPxwNUcNYei6dZt-EpX9aLNRqCY1PyYXq4kUScI`,
+        host: `preview.contentful.com`,
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-styled-components",
+  ],
 };
